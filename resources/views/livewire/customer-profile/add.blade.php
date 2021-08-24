88e23c52379a3eaf6b89888807eccc5e39b6c87d
Shehryar Amin <ibneadam388@gmail.com>
2021-08-25 01:33:14 +0500

    Add Page Done

--- resources/views/livewire/customer-profile/add.blade.php
+++ resources/views/livewire/customer-profile/add.blade.php
@@ -87,12 +87,14 @@
                             <fieldset class="space-y-5">
                                 <div class="relative flex items-start">
                                     <div class="flex items-center h-5">
-                                        <input id="comments" aria-describedby="comments-description" name="comments"
+                                        <input id="referral" aria-describedby="comments-description" name="comments"
                                                type="checkbox" wire:model="is_referral"
                                                class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded">
                                     </div>
                                     <div class="ml-3 text-sm">
-                                        <label for="comments" class="font-medium text-gray-700">Add Referral</label>
+                                        <label for="referral" class="font-medium text-gray-700">Add Referral</label>
+                                        <p id="comments-description" class="text-gray-500">Get to know about how your
+                                            business grows.</p>
                                     </div>
                                 </div>
                             </fieldset>
