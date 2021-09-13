ec11182fdef7200fba21b3ce8f9458f5ed7e67a8
Sharryy <ibneadam388@gmail.com>
2021-09-13 16:23:38 +0500

    Commented Things

--- resources/views/livewire/customer-profile/index.blade.php
+++ resources/views/livewire/customer-profile/index.blade.php
@@ -101,42 +101,42 @@
                         </tr>
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-200">
-                        <tr>
-                            <td class="px-3 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                            </td>
-                            <td class="px-3 py-4 whitespace-nowrap text-sm text-gray-500">
-                            </td>
-                            <td class="px-3 py-4 whitespace-nowrap text-sm text-gray-500">
-                            </td>
+{{--                        <tr>--}}
+{{--                            <td class="px-3 py-4 whitespace-nowrap text-sm font-medium text-gray-900">--}}
+{{--                            </td>--}}
+{{--                            <td class="px-3 py-4 whitespace-nowrap text-sm text-gray-500">--}}
+{{--                            </td>--}}
+{{--                            <td class="px-3 py-4 whitespace-nowrap text-sm text-gray-500">--}}
+{{--                            </td>--}}
 
-                            <td class="px-3 py-4 whitespace-nowrap text-sm text-gray-500">
-                            </td>
-                            <td class="px-3 py-4 whitespace-nowrap text-sm text-gray-500">
-                            </td>
-                            <td class="px-3 py-4 whitespace-nowrap text-sm font-medium">
-                                <div class="flex">
-                                    <a href="#"
-                                       class="">
-                                        <svg xmlns="http://www.w3.org/2000/svg"
-                                             class="text-blue-500 hover:text-blue-600 cursor-pointer h-5 w-4"
-                                             fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
-                                        </svg>
-                                    </a>
-                                    <span class="mx-1 text-gray-500 font-light">|</span>
-                                    <a href="#">
-                                        <svg xmlns="http://www.w3.org/2000/svg"
-                                             class="text-red-500 hover:text-red-600 cursor-pointer h-5 w-4"
-                                             fill="none"
-                                             viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
-                                        </svg>
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>
+{{--                            <td class="px-3 py-4 whitespace-nowrap text-sm text-gray-500">--}}
+{{--                            </td>--}}
+{{--                            <td class="px-3 py-4 whitespace-nowrap text-sm text-gray-500">--}}
+{{--                            </td>--}}
+{{--                            <td class="px-3 py-4 whitespace-nowrap text-sm font-medium">--}}
+{{--                                <div class="flex">--}}
+{{--                                    <a href="#"--}}
+{{--                                       class="">--}}
+{{--                                        <svg xmlns="http://www.w3.org/2000/svg"--}}
+{{--                                             class="text-blue-500 hover:text-blue-600 cursor-pointer h-5 w-4"--}}
+{{--                                             fill="none" viewBox="0 0 24 24" stroke="currentColor">--}}
+{{--                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"--}}
+{{--                                                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>--}}
+{{--                                        </svg>--}}
+{{--                                    </a>--}}
+{{--                                    <span class="mx-1 text-gray-500 font-light">|</span>--}}
+{{--                                    <a href="#">--}}
+{{--                                        <svg xmlns="http://www.w3.org/2000/svg"--}}
+{{--                                             class="text-red-500 hover:text-red-600 cursor-pointer h-5 w-4"--}}
+{{--                                             fill="none"--}}
+{{--                                             viewBox="0 0 24 24" stroke="currentColor">--}}
+{{--                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"--}}
+{{--                                                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>--}}
+{{--                                        </svg>--}}
+{{--                                    </a>--}}
+{{--                                </div>--}}
+{{--                            </td>--}}
+{{--                        </tr>--}}
                         </tbody>
                     </table>
                 </div>
